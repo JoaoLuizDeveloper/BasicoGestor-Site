6bf003735f3d9afeb02435904209676482e73296
JoaoLuizDeveloper <juninhoworld2012@gmail.com>
2020-07-10 14:30:50 -0300

    Esqueci de habilitar a criação do banco automatico no admin rs

--- QuestorSistemasAdmin/QuestorSistemasAdmin/Startup.cs
+++ QuestorSistemasAdmin/QuestorSistemasAdmin/Startup.cs
@@ -50,7 +50,7 @@ namespace QuestorSistemasAdmin
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            //db.Database.EnsureCreated();
+            db.Database.EnsureCreated();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
